ac93074724080c94d6fed3e9667fef16dc7d74f6
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-28 21:27:55 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,9 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
- def def get_courses
+
+
+  def get_courses
   self.get_page.css(".post")
   def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
